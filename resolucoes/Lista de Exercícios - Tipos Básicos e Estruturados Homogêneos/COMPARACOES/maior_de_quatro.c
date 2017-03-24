94d0692777bcf9f5a672ec0760b248f83044dd49
Kevin Oliveira <kevin.oliveira@live.com>
2017-03-24 16:35:30 -0300

    adicionado link para função externa
    função maior_de_dois
    link : https://github.com/kevinoliveira/Monitoria_LPI/blob/master/resolucoes/Lista%20de%20Exerc%C3%ADcios%20-%20Tipos%20B%C3%A1sicos%20e%20Estruturados%20Homog%C3%AAneos/COMPARACOES/maior_de_dois.c

--- resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/COMPARACOES/maior_de_quatro.c
+++ resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/COMPARACOES/maior_de_quatro.c
@@ -3,5 +3,7 @@ int maior_de_quatro(int a, int b, int c, int d){
 	maior = maior_de_dois(a,b);
 	maior = maior_de_dois(maior,c);
 	maior = maior_de_dois(maior,d);
+	//LINK PARA A FUNÇÃO maior_de_dois
+	//https://github.com/kevinoliveira/Monitoria_LPI/blob/master/resolucoes/Lista%20de%20Exerc%C3%ADcios%20-%20Tipos%20B%C3%A1sicos%20e%20Estruturados%20Homog%C3%AAneos/COMPARACOES/maior_de_dois.c
 	return maior;
 }
