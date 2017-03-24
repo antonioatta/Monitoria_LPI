b7408d2c0bdeda433087873a957b095401841baf
Kevin Oliveira <kevin.oliveira@live.com>
2017-03-24 16:32:53 -0300

    adicionando referencia a uma função externa
    função existe 
    link : https://github.com/kevinoliveira/Monitoria_LPI/blob/master/resolucoes/Lista%20de%20Exerc%C3%ADcios%20-%20Tipos%20B%C3%A1sicos%20e%20Estruturados%20Homog%C3%AAneos/COMPARACOES/existe.c

--- resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/COMPARACOES/conjuntos_iguais.c
+++ resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/COMPARACOES/conjuntos_iguais.c
@@ -2,6 +2,8 @@ int conjuntos_iguais(int a[], int b[], int c){
 	int i;
 	for(i=0;i<c;i++){
 		if(existe(a,c,a[i]) != existe(b,c,a[i])) return 0;
+		//LINK PARA A FUNÇÃO existe
+		//https://github.com/kevinoliveira/Monitoria_LPI/blob/master/resolucoes/Lista%20de%20Exerc%C3%ADcios%20-%20Tipos%20B%C3%A1sicos%20e%20Estruturados%20Homog%C3%AAneos/COMPARACOES/existe.c
 	}
 	return 1;
 }
