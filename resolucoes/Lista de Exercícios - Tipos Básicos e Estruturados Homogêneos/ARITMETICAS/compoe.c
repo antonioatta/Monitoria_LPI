c6f5d850d37772bd92ead08ac618f19a959137da
Kevin Oliveira <kevin.oliveira@live.com>
2017-03-24 17:35:39 -0300

    atualizando função compõe

--- resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/ARITMETICAS/compoe.c
+++ resolucoes/Lista de Exercícios - Tipos Básicos e Estruturados Homogêneos/ARITMETICAS/compoe.c
@@ -1,7 +1,12 @@
-float compoe(int a, int b){
+float compoe(int a, int b, int d){
 	float c = (float) b;
+	//movendo uma casa para direita até que o numero seja menor que 1
 	while(c>1) c*=0.1;
+	//"adicionado" zeros apos a virgula, movendo d casas para direita
+	while(d>0){
+		c*=0.1;
+		d--;
+	}
 	c+=(float)a;
 	return c;
 }
-
